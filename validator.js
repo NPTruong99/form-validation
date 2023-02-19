1cbcfef4e7b5c4b77963b6c23fa682793d46207d
unknown <truongnp29@gmail.com>
2023-02-19 22:21:05 +0700

    Thêm rules, xử lý submit  form

--- validator.js
+++ validator.js
@@ -1,11 +1,24 @@
 // Đối tượng `Validator`
 function Validator(options) {
+    let selectorRules = {};
+
     // Hàm thực hiện validate
     function validate(inputElement, rule) {
-        let errorMessage = rule.test(inputElement.value);
+        let errorMessage;
         let errorElement = inputElement.parentElement.querySelector(
             options.errorSelector
         );
+
+        // Lấy ra các rule của selector
+        let rules = selectorRules[rule.selector];
+
+        // Lặp qua từng rule & kiểm tra
+        // Nếu có lỗi thì dừng kiểm tra
+        for (let i = 0; i < rules.length; i++) {
+            errorMessage = rules[i](inputElement.value);
+            if (errorMessage) break;
+        }
+
         if (errorMessage) {
             errorElement.innerText = errorMessage;
             inputElement.parentElement.classList.add("invalid");
@@ -13,13 +26,61 @@ function Validator(options) {
             errorElement.innerText = "";
             inputElement.parentElement.classList.remove("invalid");
         }
+
+        return !errorMessage;
     }
 
     // Lấy element của form cần validate
     let formElement = document.querySelector(options.form);
 
     if (formElement) {
+        // Khi submit form
+        formElement.onsubmit = function (e) {
+            e.preventDefault();
+
+            let isFormValid = true;
+
+            // Lặp qua từng rules và Validate
+            options.rules.forEach(function (rule) {
+                let inputElement = formElement.querySelector(rule.selector);
+                let isValid = validate(inputElement, rule);
+                if (!isValid) {
+                    isFormValid = false;
+                }
+            });
+
+            if (isFormValid) {
+                // Trường hợp submit với Javascript
+                if (typeof options.onSubmit === "function") {
+                    let enableInput = formElement.querySelectorAll("[name]");
+
+                    let formValue = Array.from(enableInput).reduce(function (
+                        values,
+                        input
+                    ) {
+                        values[input.name] = input.value;
+                        return values;
+                    },
+                    {});
+
+                    options.onSubmit(formValue);
+                }
+                // Trường hợp submit với hành vi mặc định
+                else {
+                    formElement.onsubmit();
+                }
+            }
+        };
+
+        // Lặp qua từng rules và xử lý
         options.rules.forEach(function (rule) {
+            // Lưu lại các rule cho mỗi input
+            if (Array.isArray(selectorRules[rule.selector])) {
+                selectorRules[rule.selector].push(rule.test);
+            } else {
+                selectorRules[rule.selector] = [rule.test];
+            }
+
             let inputElement = formElement.querySelector(rule.selector);
             let errorElement = inputElement.parentElement.querySelector(
                 options.errorSelector
@@ -72,6 +133,17 @@ Validator.minLength = function (selector, min) {
             return value.length >= min
                 ? undefined
                 : `Password must be at least ${min} characters long`;
+        },
+    };
+};
+
+Validator.isConfirmed = function (selector, getConfirmValue) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value === getConfirmValue()
+                ? undefined
+                : "The password confirmation does not match";
         },
     };
 };
